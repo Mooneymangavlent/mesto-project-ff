43c67c2997ea0c85562570701fa4a687bc41190d
Ivan Gusev <ivan.yrist@yandex.ru>
2024-02-21 18:48:49 +0300

    Project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+"use strict";
 
-// @todo: DOM узлы
+const template = document.querySelector("#card-template");
+const gallery = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+function removeElement(element) {
+  element.closest(".card").remove();
+}
 
-// @todo: Функция удаления карточки
+function updateCard(cardElement, cardData) {
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.title;
+}
 
-// @todo: Вывести карточки на страницу
+function addCards(card) {
+  const newCard = template.content.cloneNode(true);
+
+  updateCard(newCard, card);
+
+  newCard.querySelector(".card").addEventListener("click", (event) => {
+    if (event.target.classList.contains("card__delete-button")) {
+      removeElement(event.target);
+    }
+  });
+
+  gallery.append(newCard);
+}
+
+initialCards.forEach((element) => {
+  addCards(element);
+});
